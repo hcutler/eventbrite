f761b82184580d26c84c3e766df629138f94ef69
Xiaodi Yu <xiaodi.yu@ou.edu>
2015-03-23 15:55:39 -0500

    Add script to pull category data from the API

--- eventbrite/events/models.py
+++ eventbrite/events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    cid = models.IntegerField(verbose_name='ID from Eventbrites', unique=True)
+    name = models.CharField(max_length=128)
+    name_localized = models.CharField(max_length=128)
+    short_name = models.CharField(max_length=128)
+    short_name_localized = models.CharField(max_length=128)
+    uri = models.URLField()
+
+    def __str__(self):
+        return self.name
+
