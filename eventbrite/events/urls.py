eb939588e19230a7b402fa35ebb2d6e9bfc67a25
Xiaodi Yu <xiaodi.yu@ou.edu>
2015-03-23 17:41:45 -0500

    Add a evnet list view

--- eventbrite/events/urls.py
+++ eventbrite/events/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from .views import CategorySelectView
+from .views import CategorySelectView, EventListView
 
 urlpatterns = patterns('',
-    url(r'^$', CategorySelectView.as_view(), name='category-list')
+    url(r'^$', CategorySelectView.as_view(), name='category-list'),
+    url(r'^eventlist/', EventListView.as_view(), name='event-list')
 )
--- eventbrite/events/views.py
+++ eventbrite/events/views.py
@@ -1,7 +1,35 @@
-from django.views.generic import FormView
+import requests
+
+from django.views.generic import FormView, TemplateView
 from .forms import CategorySelectForm
 
+EVENT_SEARCH_API = 'https://www.eventbriteapi.com/v3/events/search/?token=BKKRDKVUVRC5WG4HAVLT'
 
 class CategorySelectView(FormView):
     template_name = 'events/category_list.html'
     form_class = CategorySelectForm
+
+
+class EventListView(TemplateView):
+    template_name = 'events/event_list.html'
+
+    def get_list(self, page=1):
+        resp_json = None
+        request_url = EVENT_SEARCH_API + "&page=%s" % page
+        resp = requests.get(request_url)
+        if resp.status_code == 200:
+            resp_json = resp.json()
+        return resp_json
+
+    def get_context_data(self, **kwargs):
+        context = super(EventListView, self).get_context_data(**kwargs)
+        page = self.request.GET.get('page', 1)
+        repo_json = self.get_list(page=page)
+        pagination = repo_json['pagination']
+        if pagination['page_number'] < pagination['page_size']:
+            context['next_page'] = pagination['page_number'] + 1
+        if pagination['page_number'] > 1:
+            context['previous_page'] = pagination['page_number'] - 1
+        event_list = repo_json['events']
+        context['event_list'] = event_list
+        return context
